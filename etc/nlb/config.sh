50292464788728fb2666646d5f6da7ee9274c792
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-06-24 12:01:17 +0200

    Remove unused variables

--- etc/nlb/config.sh
+++ etc/nlb/config.sh
@@ -18,12 +18,7 @@ NLB_CONFIG_H="/etc/nlb/config.sh included";
 ##	definitions							      ##
 ################################################################################
 NLB_COMPOSE_FPATH="etc/docker/swarm/docker-compose.yaml";
-NLB_PORT_EXP=32080;
-NLB_PORT_RC=31080;
-NLB_PORT_STABLE=30080;
-NLB_STABILITY="exp";	## "stable", "rc", or "exp"
 NLB_STACK_BASENAME="nlb";
-NLB_VERSION="master";	## tag name or branch name.
 
 
 ################################################################################
